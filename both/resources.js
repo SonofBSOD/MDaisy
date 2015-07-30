5dbbc62efb1653767263c2dbb1ddaa950efc79f4
Jai Chaudhary <jai.chaudhary.iitd@gmail.com>
2015-07-29 22:59:05 -0400

    Fixed Site link for MP3 resources

--- both/resources.js
+++ both/resources.js
@@ -1,4 +1,4 @@
-SITE_LINK = "http://mdaisytest.meteor.com/";
+SITE_LINK = "https://web.daisyhealth.io/";
 BEEP_LINK = SITE_LINK + "/beep.mp3";
 DING_LINK = SITE_LINK + "/ding.wav";
 DRIP_LINK = SITE_LINK + "/drip.wav";
