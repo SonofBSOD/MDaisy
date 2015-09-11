c030f4168a3df84b8585a72dfc13986149234ba6
yuchien <raphaeltnfsh@gmail.com>
2015-09-10 21:08:04 -0400

    Finished the filter feature

--- client/templates/staff_search_box.js
+++ client/templates/staff_search_box.js
@@ -18,27 +18,52 @@
 var getQuery = function(){
 	var query = {};
 	query.procs = []
+	query.procs_id = []
 	query.organization = ""
+	query.organization_id = ""
 	query.text = ""
 
 	$("#proc_filter span.proc_selected").each(function(id, proc){
 		query.procs.push( $(proc).text() )
+		query.procs_id.push( $(proc).data("id") )
 	})
 
 	if(query.procs.length === 0){
 		$("#proc_filter span").each(function(id, proc){
 			query.procs.push( $(proc).text() )
+			query.procs_id.push( $(proc).text() )
 		})
-
 	}
 
 	query.text = $("#search_box .search-box").val();
-	query.organization = $("#organization_filter select").val();
+	query.organization = $("#organization_filter :selected").text();
+	query.organization_id = $("#organization_filter select").val();
 
 	return query;
 
 }
 
+
+
+Template.staff_search_box.onRendered(function () {
+	var query = Session.get("query");
+	if(query){
+
+		if(query.organization_id){
+			$("#organization_filter select").val(query.organization_id)	
+		}
+		if(query.text){
+			$("#search_box input").val(query.text)
+		}
+		if(query.procs_id){
+			for (var i = 0; i < query.procs_id.length; i++) {
+				
+				$("#proc_filter [data-id='" + query.procs_id[i] + "']").addClass("proc_selected")
+
+			};		
+		}
+	}
+});
 
 Template.staff_search_box.events({
 	"keyup .search-box" : function(e){
